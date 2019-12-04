1bf3da6fe2b8d82755dc9c1d664c01719d2d4f8e
eamiear <justcall_me_k@163.com>
2019-12-04 11:07:36 +0800

    构建链接修改

--- src/common/config.js
+++ src/common/config.js
@@ -2,7 +2,7 @@
  * @Author: eamiear
  * @Date: 2019-02-19 10:38:23
  * @Last Modified by: eamiear
- * @Last Modified time: 2019-11-18 10:59:47
+ * @Last Modified time: 2019-12-04 11:06:26
  */
 
 const config = {
@@ -10,8 +10,8 @@ const config = {
     baseApi: 'http://localhost:8080/'
   },
   prod: {
-    baseApi: 'https://192.168.200.254'
-    // baseApi: 'https://aliiot.on-bright.com'
+    // baseApi: 'https://192.168.200.254'
+    baseApi: 'https://aliiot.on-bright.com'
   }
 }
 export default config
