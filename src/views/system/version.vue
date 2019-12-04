bef8a9ce6ba854aa2315968e9d9870d34ca8c6fc
eamiear <justcall_me_k@163.com>
2019-12-04 14:18:12 +0800

    删除成功，刷新列表

--- src/views/system/version.vue
+++ src/views/system/version.vue
@@ -256,6 +256,7 @@ export default {
             type: 'success',
             message: '删除成功'
           })
+          this.getVersionList()
         } else {
           this.$message({
             type: 'error',
