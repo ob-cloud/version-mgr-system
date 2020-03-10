db6e7c085eb622120952fae58ad9edaaf96b60ca
eamiear <justcall_me_k@163.com>
2020-03-10 11:15:14 +0800

    修复文件多次上次问题

--- src/common/config.js
+++ src/common/config.js
@@ -2,16 +2,16 @@
  * @Author: eamiear
  * @Date: 2019-02-19 10:38:23
  * @Last Modified by: eamiear
- * @Last Modified time: 2019-12-04 11:06:26
+ * @Last Modified time: 2020-03-03 15:28:24
  */
 
 const config = {
   dev: {
-    baseApi: 'http://localhost:8080/'
+    baseApi: 'http://localhost:8080/',
   },
   prod: {
-    // baseApi: 'https://192.168.200.254'
-    baseApi: 'https://aliiot.on-bright.com'
+    baseApi: ''
+    // baseApi: 'https://aliiot.on-bright.com'
   }
 }
 export default config
